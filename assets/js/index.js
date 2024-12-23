c591ad342b224ab16aa2e0365943df85dc0147ba
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-12-23 18:10:46 +0530

    first commant

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,53 @@ $('.banner-carousel').owlCarousel({
 
 
 
+$('.service-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,              
+    autoplayTimeout: 3000,      
+    autoplayHoverPause: true, 
+    navText: [
+        '<span><i class="fas fa-long-arrow-alt-left"></i> Prev</span>', // Previous button
+        '<span>Next <i class="fas fa-long-arrow-alt-right"></i></span>' // Next button
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+// Gallery-slide-start
+$('.gallery_slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+        '<span><i class="fas fa-long-arrow-alt-left"></i> Prev</span>', // Previous button
+        '<span>Next <i class="fas fa-long-arrow-alt-right"></i></span>' // Next button
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
 let nav = document.querySelector(".navbar");
 window.onscroll = function (){
     if(document.documentElement.scrollTop > 20){
